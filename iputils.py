8f46e7853870bc5cfdb2f4fa2b254647cb4e9930
unknown <adgi.xx@gmail.com>
2019-07-31 22:28:47 +1000

    first commit

---
+++ iputils.py
@@ -0,0 +1,34 @@
+git import ipaddress
+
+#zones
+zone1 = ['192.168.0.0/24','192.168.1.0/24','172.16.0.0/24','172.16.1.0/24']
+zone2 = ['192.168.2.0/24','192.168.3.0/24','172.16.2.0/24','172.16.3.0/24']
+
+#environments
+prod = ['192.168.0.0/23','192.168.2.0/23']
+nonprod = ['172.16.0.0/23','172.16.2.0/23']
+
+#takes a string as an input, runs IP address query to map it to a variable (one of the zones above)
+def zonelookup(ip):
+    for i in zone1:
+        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
+            return 'Zone1'
+    for i in zone2:     
+        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
+            return 'Zone2'        
+
+#takes a string as an input, runs IP address query to map it to a variable (one of the environemnts above)
+def envlookup(ip):
+    for i in prod:
+        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
+            return 'Production'
+    for i in nonprod:
+        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
+            return 'Non-Production'    
+
+#use this function to define permitted and not permitted flows
+def flowlogic(source,dest):
+    if source == dest:
+        return 'Flow Permitted'
+    else:
+        return 'Flow NOT permitted!'
---
+++ main.py
@@ -0,0 +1,16 @@
+from iputils import *
+import ipaddress
+
+#grab source/dest inputs
+ipsource = input('Source? ')
+ipdestination = input('Destination? ')
+#lookup zones and environments
+source_zone = zonelookup(ipsource)
+source_env = envlookup(ipsource)
+destination_zone = zonelookup(ipdestination)
+destination_env = envlookup(ipdestination)
+#print statements as outputs
+print('\nSource Queried: ' + ipsource + '\nZone: ' + source_zone + '\nEnvironment: ' + source_env)
+print('\nDestination Queried: ' + ipdestination + '\nZone: ' + destination_zone + '\nEnvironment: ' + destination_env) 
+print('\nFlow Analysis\n-------------\nZone Flow: ' + source_zone + ' -> ' + destination_zone + ' - ' + flowlogic(source_zone,destination_zone))
+print('Environment Flow: ' + source_env + ' -> ' + destination_env + ' - ' + flowlogic(source_env,destination_env))

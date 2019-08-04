e06b8a8c79ccb246500faa3e0fc43fabc3c9ce31
unknown <adgi.xx@gmail.com>
2019-08-04 11:47:09 +1000

    added args and modified variable creation method

--- iputils.py
+++ iputils.py
@@ -1,45 +1,47 @@
 import ipaddress
 import csv
 
-#zones
-zone1 = []
-zone2 = []
-
-#environments
-prod = []
-nonprod = []
-
 #create variables from a csv
-result = {}
+zone = {}
+environment = {}
 
-with open('subnets.csv','r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(csvreader, None)
-    for row in csvreader:
-        if row[1] in result:
-            result[row[1]].append(row[0])
-        else:
-            result[row[1]] = [row[0]]    
+def zoneparse(csvfile):
+    with open(csvfile,'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(csvreader, None)
+        for row in csvreader:
+            if row[1] in zone:
+                zone[row[1]].append(row[0])
+            else:
+                zone[row[1]] = [row[0]]
 
-print(result)
+def envparse(csvfile):
+    with open(csvfile,'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(csvreader, None)                    
+        for row in csvreader:
+            if row[2] in environment:
+                environment[row[2]].append(row[0])
+            else:
+                environment[row[2]] = [row[0]]  
 
 #takes a string as an input, runs IP address query to map it to a variable (one of the zones above)
 def zonelookup(ip):
-    for i in zone1:
-        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
-            return 'Zone1'
-    for i in zone2:     
-        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
-            return 'Zone2'        
+    for i in zone:
+        result = zone.get(i)
+        for b in result:
+            if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(b):
+                return(i)
+           
 
 #takes a string as an input, runs IP address query to map it to a variable (one of the environemnts above)
 def envlookup(ip):
-    for i in prod:
-        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
-            return 'Production'
-    for i in nonprod:
-        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(i):
-            return 'Non-Production'    
+     for i in environment:
+        result = environment.get(i)
+        for b in result:
+            if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(b):
+                return(i)    
+        
 
 #use this function to define permitted and not permitted flows
 def flowlogic(source,dest):
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from iputils import *
+import argparse
 
-#grab source/dest inputs
-ipsource = input('Source? ')
-ipdestination = input('Destination? ')
+parser = argparse.ArgumentParser(description='lookup source and destination against subnet lists')
+parser.add_argument('-csv',action='store',help='Specify csv file to look ip addresses up against')
+parser.add_argument('-source',action='store',help='Specify source IP to look up')
+parser.add_argument('-dest',action='store',help='Specify destination IP to look up')
+args=parser.parse_args()
+
+#set source/dest inputs
+ipsource = args.source
+ipdestination = args.dest
+
+#generate zones and environments
+zoneparse(args.csv)
+envparse(args.csv)
+
 #lookup zones and environments
 source_zone = zonelookup(ipsource)
 source_env = envlookup(ipsource)
 destination_zone = zonelookup(ipdestination)
 destination_env = envlookup(ipdestination)
+
 #print statements as outputs
-print('\nSource Queried: ' + ipsource + '\nZone: ' + source_zone + '\nEnvironment: ' + source_env)
+print('-'*20)
+print('Address Mapping')
+print('-'*20)
+print('Source Queried: ' + ipsource + '\nZone: ' + source_zone + '\nEnvironment: ' + source_env)
 print('\nDestination Queried: ' + ipdestination + '\nZone: ' + destination_zone + '\nEnvironment: ' + destination_env) 
-print('\nFlow Analysis\n-------------\nZone Flow: ' + source_zone + ' -> ' + destination_zone + ' - ' + flowlogic(source_zone,destination_zone))
+print('\n' + ('-'*20))
+print('Flow analysis')
+print('-'*20)    
 print('Environment Flow: ' + source_env + ' -> ' + destination_env + ' - ' + flowlogic(source_env,destination_env))
+print('Zone Flow: ' + source_zone + ' -> ' + destination_zone + ' - ' + flowlogic(source_zone,destination_zone))
